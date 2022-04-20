f64969d80f299a5fdc98cf91980e6fb4075074a6
M4isv <boladaomailson@gmail.com>
2022-04-20 09:21:39 -0300

    Create alert.js

---
+++ source/js/alert.js
@@ -0,0 +1,15 @@
+const text_complete=document.querySelector("#text_complete")
+
+text_complete.addEventListener("click",function(){
+	swal({
+	title: `
+There was once a farmer and his wife. She had been 
+married before, her first husband had died, and now 
+she was married again. They lived on a remote farm in 
+the west of England, and what a pair of fools they 
+were! Which of them was the most foolish? Listen to 
+the story and decide for yourself.
+		
+		`,
+	});
+})
